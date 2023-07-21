dbf4cac78ef592a08c77a9b6a3e8a202ea17eb3c
Fabio Lucas <fabiolucs19@gmail.com>
2023-07-20 21:23:14 -0300

    implementing list by status

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -6,18 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
     @Autowired
     private PedidoRepository pedidoRepository;
 
-    @GetMapping("/home")
+    @GetMapping
     public String home(Model model) {
         List<Pedido> listaPedidos = pedidoRepository.findAll();
+        model.addAttribute("pedidos", listaPedidos);
+
+        return "home";
+    }
+
+    @GetMapping("/{status}")
+    public String listByStatus(@PathVariable("status") String status, Model model) {
+        List<Pedido> listaPedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
         model.addAttribute("pedidos", listaPedidos);
 
         return "home";
