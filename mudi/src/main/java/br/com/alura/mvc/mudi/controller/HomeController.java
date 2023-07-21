7f985b08399ca08cdc889f52fda14f72fe100b2f
Fabio Lucas <fabiolucs19@gmail.com>
2023-07-20 22:31:42 -0300

    fixing some format errors

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +33,12 @@ public class HomeController {
     public String listByStatus(@PathVariable("status") String status, Model model) {
         List<Pedido> listaPedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
         model.addAttribute("pedidos", listaPedidos);
-
+        model.addAttribute("status", status);
         return "home";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String onError(){
+        return "redirect:/home";
     }
 }
